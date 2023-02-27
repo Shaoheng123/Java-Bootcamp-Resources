ea213005f230680d6059b22d94772b2201c59670
Shaoheng123 <56971971+Shaoheng123@users.noreply.github.com>
2023-02-27 16:42:18 +0800

    WB2.2 done

--- Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
+++ Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -1,5 +1,12 @@
 public class YourInitials {
     public static void main(String[] args) {
+        System.out.println(" SSS   H   H");
+        System.out.println("S   S  H   H");
+        System.out.println("S      H   H");
+        System.out.println(" SSS   HHHHH");
+        System.out.println("    S  H   H");
+        System.out.println("S   S  H   H");
+        System.out.println(" SSS   H   H");
          // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
     }
 }
--- Module 1 - Introduction to Java/2. Variables/Workbook 2.1/Points.java
+++ Module 1 - Introduction to Java/2. Variables/Workbook 2.1/Points.java
@@ -7,17 +7,22 @@ public class Points {
 
         System.out.println("Harry was caught wandering the halls. -50 points for Gryffindor");
         //update points here
+        points -= 50;
         System.out.println("Harry was being cheeky in class. -3 points for Gryffindor");
+        points -= 3;
         //update points here
         System.out.println("Hermione got full marks on Lockhart's quiz. 30 points for Gryffindor");
         //update points here
+        points += 30;
         System.out.println("Ron won the underground chess game. 100 points for Gryffindor");
+        points+=100;
         //update points here
         System.out.println("Harry defeated Quirrell. 60 points for Gryffindor");
+        points += 60;
         //update points here        
         
         // Compare your result to what's on Learn the Part.
-        
+        System.out.println(points);
     }
 }
 
