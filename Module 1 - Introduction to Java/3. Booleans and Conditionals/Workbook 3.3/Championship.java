f1f62b54897f1155f95c25e1aee0c1095d666027
Shaoheng123 <56971971+Shaoheng123@users.noreply.github.com>
2023-02-28 17:46:21 +0800

    If Else Conditional  Completed

--- Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.3/Championship.java
+++ Module 1 - Introduction to Java/3. Booleans and Conditionals/Workbook 3.3/Championship.java
@@ -5,8 +5,17 @@ public class Championship {
 
         int gryffindor = 400;    //gryffindor points
         int ravenclaw = 200;    //ravenclaw points
-
-        // int margin = amount of points by which gryffindor scored over ravenclaw;
         
+        // int margin = amount of points by which gryffindor scored over ravenclaw;
+        int margin = gryffindor - ravenclaw;
+        if (gryffindor - ravenclaw >= 300) {
+            System.out.println("gryffindor wins the championship");
+        } else if (gryffindor - ravenclaw >= 0) {
+            System.out.println("gryffindor came in second");
+        } else if (gryffindor - ravenclaw <= 100) {
+            System.out.println("gryffindor came in third");
+        } else {
+            System.out.println("gryffindor came in last");
+        }
     }
 }
