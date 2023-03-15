852043a14f643053ee9f5e3a3ac38d6facedb27a
Shaoheng123 <56971971+Shaoheng123@users.noreply.github.com>
2023-03-15 17:24:05 +0800

    Completed Arrays, doing capstone hangman project

--- Module 1 - Introduction to Java/6. Arrays/Workbook 6.7/RingAnnouncer.java
+++ Module 1 - Introduction to Java/6. Arrays/Workbook 6.7/RingAnnouncer.java
@@ -3,10 +3,19 @@ public class RingAnnouncer {
         String[] record = {"WIN", "WIN", "WIN", "LOSS", "WIN", "WIN", "LOSS"};
         int wins = 0;
         int losses = 0;
+        for (int i = 0; i < record.length; i++) {
+            if (record[i].equals("WIN")) {
+                wins=wins+1;
+            }
+            else{
+                losses=losses+1;
+            }
+            }
+        
         
         // Instructions for this workbook are on Learn the Part (Workbook 6.7).
         
-        System.out.println("\nWith a professional record of <wins> Wins and <losses> losses.");
+        System.out.println("\nWith a professional record of "+wins+"Wins" +losses+"losses.");
         System.out.println("He is the pride of oracle: Java Fury!");
 
 
